e841ea13d443eeead77fab65f8cbb01a3d2d2a91
Devin Jacobs <dbuttonz@gmail.com>
2020-02-16 12:54:08 -0700

    correct_guess_ordered

--- src/Mastermind.jl
+++ src/Mastermind.jl
@@ -1,5 +1,67 @@
 module Mastermind
 
-greet() = print("Hello World!")
+export Correctness, correct, misplaced, wrong
+export correct_guess_ordered
+
+include("Bags.jl"); using .Bags
+
+using Random
+using Test: @test
+
+@enum Correctness correct misplaced wrong
+
+const Guess = Tuple{Int, Correctness}
+
+function correct_guess_ordered(secret, guess)
+    @assert length(secret) == length(guess)
+    corrected_guess = Vector{Union{Nothing, Guess}}(nothing, length(guess))
+    unused_secrets = Bag(secret)
+    unused_guesses = Bag(guess)
+    for i in 1:length(guess)
+        if secret[i] == guess[i]
+            corrected_guess[i] = (guess[i], correct)
+            remove!(unused_secrets, secret[i])
+            remove!(unused_guesses, guess[i])
+        end
+    end
+    while true
+        unused_intersection = intersect(unused_secrets, unused_guesses)
+        length(unused_intersection) == 0 && break
+        misplaced_color = unused_intersection[1]
+        for i in shuffle(1:length(guess))
+            if guess[i] == misplaced_color && isnothing(corrected_guess[i])
+                corrected_guess[i] = (misplaced_color, misplaced)
+                remove!(unused_secrets, misplaced_color)
+                remove!(unused_guesses, misplaced_color)
+                break
+            end
+        end
+    end
+    for i in 1:length(guess)
+        if isnothing(corrected_guess[i])
+            corrected_guess[i] = (guess[i], wrong)
+        end
+    end
+    corrected_guess
+end
+
+function test_correct_guess_ordered()
+    @test correct_guess_ordered([1, 2, 3, 4], [1, 2, 3, 4]) == [(1, correct), (2, correct), (3, correct), (4, correct)]
+    @test correct_guess_ordered([1, 2, 3, 4], [1, 2, 3, 3]) == [(1, correct), (2, correct), (3, correct), (3, wrong)]
+    @test correct_guess_ordered([1, 2, 3, 4], [5, 5, 5, 5]) == [(5, wrong), (5, wrong), (5, wrong), (5, wrong)]
+    matched = 0
+    for _ in 1:100
+        if correct_guess_ordered([1, 2, 2, 2], [3, 3, 1, 1]) == [(3, wrong), (3, wrong), (1, misplaced), (1, wrong)]
+            matched += 1
+        end
+    end
+    @test 10 <= matched <= 90
+end
+
+function test()
+    test_correct_guess_ordered()
+end
+
+test()
 
 end # module
